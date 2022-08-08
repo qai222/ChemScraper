5fa4853cc96bce39f74667c7af4a33a5760d4b09
qai <qai1@fordham.edu>
2022-08-08 06:56:39 -0400

    organize import

---
+++ ChemScraper/__init__.py
@@ -0,0 +1,5 @@
+from ChemScraper.pubchem import *
+from ChemScraper.schema import Compound
+from ChemScraper.settings import *
+from ChemScraper.utils import *
+from ChemScraper.vscraper import *
---
+++ ChemScraper/vscraper/__init__.py
@@ -0,0 +1,2 @@
+from ChemScraper.vscraper.sigma_aldrich import get_sigma_aldrich_patable, get_sigma_aldrich_patables, \
+    sigma_sds_url_from_product_url, get_chrome_driver
